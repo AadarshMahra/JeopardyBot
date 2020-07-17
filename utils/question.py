00ed42077d78378d9c6edb23b40d987c9d7b5c70
AadarshMahra <aadarsh.mahra@gmail.com>
2020-07-17 00:02:01 -0700

    value is now stored as an int

--- utils/question.py
+++ utils/question.py
@@ -4,15 +4,17 @@ import discord
 class QuestionPanel:
     def __init__(self, category,  value, question, answer):
         self.category = category
-        self.value = value
+        self.value = int(value[1:])
         self.question = question
         self.answer = answer
 
     def get_answer(self):
         return self.answer
+    def get_value(self):
+        return self.value
 
 # displays Question panel onto screen
     def get_embed(self):
         embed = discord.Embed(title=f'Category: {self.category}', description=self.question, inline=False)
-        embed.add_field(name='Value', value=self.value, inline=False)
+        embed.add_field(name='Value', value="$"+str(self.value), inline=False)
         return embed
