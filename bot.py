30d24f4ffe46f9fea51cf90ddf427165168f41b5
dMahra <54589314+dMahra@users.noreply.github.com>
2020-12-06 01:50:19 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext.commands import CommandNotFound
 from utils.functions import fetch_random_panel as frp
 from utils.functions import jaro_winkler
 import time
-# wassup 
+# wassup
 #  global dict to keep track of user score in every server
 #  key: (username, server id), value: user score in that server
 scores = {}
